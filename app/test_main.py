6d1664cf4ba3acbba3ec679d37bb2a4fab091457
Danylo Povar <danila74povar@gmail.com>
2026-01-15 14:06:16 +0200

    Solution cat and dog

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zeros_for_zero_age() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_return_zeros_when_age_less_than_15() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_one_when_age_between_15_and_24() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_two_when_age_is_24() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_calculate_correctly_after_second_year() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_should_work_with_large_numbers() -> None:
+    assert get_human_age(100, 100) == [21, 17]
